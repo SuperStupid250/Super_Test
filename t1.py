9055551784f78de0445fce190ae26bb83add5b19
SuperStupid250 <2409625317@qq.com>
2022-01-14 18:28:34 +0800

    chore(增加写入csv功能)

--- t1.py
+++ t1.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import requests
+import requests, csv
 import time
 url = 'http://192.168.66.219/pan/nodes'
 driver = webdriver.Chrome()
@@ -9,6 +9,9 @@ driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('Lock4Safe')
 driver.find_element_by_xpath('/html/body/div/div/div/div/div[2]/form/button').click()
 
 time.sleep(5)
+f = open('C:/Users/Administrator/Desktop/123.csv', mode='a+', encoding='utf-8')
+csv_writer = csv.writer(f)
+
 
 a = driver.find_elements_by_xpath('//div[@class="card-body"]/table//tr/td[1]')
 b = driver.find_elements_by_xpath('//div[@class="card-body"]/table//tr/td[5]/span')
@@ -17,6 +20,9 @@ for i,j in zip(a,b):
     title = i.text
     list = j.get_attribute('class')
     print(title +'----' +list)
+    csv_writer.writerow([title,list])
+
+f.close()
 
 
 
